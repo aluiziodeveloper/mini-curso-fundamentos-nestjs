5b31e996c576c0b161b38e43aa31af6f8209a4e6
Aluizio Developer <aluiziodeveloper@gmail.com>
2021-06-18 17:43:44 -0300

    Status code

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,14 +1,29 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  HttpCode,
+  HttpStatus,
+  Res,
+} from '@nestjs/common';
 
 @Controller('courses')
 export class CoursesController {
   @Get()
-  findAll() {
-    return 'Listagem de cursos';
+  findAll(@Res() response) {
+    return response.status(200).send('Listagem de cursos');
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
     return `Curso #${id}`;
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.NO_CONTENT)
+  create(@Body('name') body) {
+    return body;
   }
 }
