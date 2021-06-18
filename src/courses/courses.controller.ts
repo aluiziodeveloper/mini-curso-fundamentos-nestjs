3dfed679564007f3c985b59e5de164d20fdae6af
Aluizio Developer <aluiziodeveloper@gmail.com>
2021-06-18 17:23:49 -0300

    Parametros de rotas

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
-@Controller()
+@Controller('courses')
 export class CoursesController {
-  @Get('courses')
+  @Get()
   findAll() {
     return 'Listagem de cursos';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `Curso #${id}`;
   }
 }
