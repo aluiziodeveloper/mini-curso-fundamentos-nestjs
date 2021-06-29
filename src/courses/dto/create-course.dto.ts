41dc899521d2a828c9e172f05642a4cd3ff5d2ca
Aluizio Developer <aluiziodeveloper@gmail.com>
2021-06-29 10:40:44 -0300

    Validacao de dados de requisicao - parte 1

--- src/courses/dto/create-course.dto.ts
+++ src/courses/dto/create-course.dto.ts
@@ -1,5 +1,12 @@
+import { IsString } from 'class-validator';
+
 export class CreateCourseDto {
+  @IsString()
   readonly name: string;
+
+  @IsString()
   readonly description: string;
+
+  @IsString({ each: true })
   readonly tags: string[];
 }
--- src/courses/dto/update-course.dto.ts
+++ src/courses/dto/update-course.dto.ts
@@ -1,5 +1,4 @@
-export class UpdateCourseDto {
-  readonly name?: string;
-  readonly description?: string;
-  readonly tags?: string[];
-}
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateCourseDto } from './create-course.dto';
+
+export class UpdateCourseDto extends PartialType(CreateCourseDto) {}
