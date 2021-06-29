cfc7b82796ed4dde6f883f88982281043ff841ee
Aluizio Developer <aluiziodeveloper@gmail.com>
2021-06-29 11:01:04 -0300

    Validacao de dados de requisicao - parte 2

--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -31,6 +31,7 @@ export class CoursesService {
 
   create(createCourseDto: any) {
     this.courses.push(createCourseDto);
+    return createCourseDto;
   }
 
   update(id: string, updateCourseDto: any) {
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,13 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
