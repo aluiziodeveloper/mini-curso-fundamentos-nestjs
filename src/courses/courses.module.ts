0e89c0774b2550f10ee2b80679a2018a747d1b88
Aluizio Developer <aluiziodeveloper@gmail.com>
2021-07-04 00:10:47 -0300

    Entity Course

--- src/courses/courses.module.ts
+++ src/courses/courses.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CoursesController } from './courses.controller';
 import { CoursesService } from './courses.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Course } from './entities/course.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Course])],
   controllers: [CoursesController],
   providers: [CoursesService],
 })
--- src/courses/entities/course.entity.ts
+++ src/courses/entities/course.entity.ts
@@ -1,6 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('courses')
 export class Course {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   name: string;
+
+  @Column()
   description: string;
+
+  @Column('json', { nullable: true })
   tags: string[];
 }
