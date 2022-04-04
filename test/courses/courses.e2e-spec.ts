43817d2e3cbca01f05b3cde4006325ee60d37f8b
Aluizio Developer <aluiziodeveloper@gmail.com>
2022-04-04 18:25:55 -0300

    Tests e2e - Courses tests

--- test/courses/courses.e2e-spec.ts
+++ test/courses/courses.e2e-spec.ts
@@ -1,14 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from '../app.module';
+import {CoursesModule} from '../../src/courses/courses.module'
 
-describe('AppController (e2e)', () => {
+describe('Courses: /courses', () => {
   let app: INestApplication;
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [CoursesModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
@@ -19,10 +19,5 @@ describe('AppController (e2e)', () => {
     await app.close();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
-  });
+  it.todo('Create POST /courses');
 });
