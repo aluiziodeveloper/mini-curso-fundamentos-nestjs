ec5fcaba80198a12a58efa77e27cbd7985337c03
Aluizio Developer <aluiziodeveloper@gmail.com>
2022-04-04 12:08:47 -0300

    CourseService tests file

---
+++ src/courses/courses.service.spec.ts
@@ -0,0 +1,18 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { CoursesService } from './courses.service';
+
+describe('CoursesService', () => {
+  let service: CoursesService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [CoursesService],
+    }).compile();
+
+    service = module.get<CoursesService>(CoursesService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
