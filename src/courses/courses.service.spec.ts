0c17515e356bd7fb4e631ff1e7767671d34f5d0a
Aluizio Developer <aluiziodeveloper@gmail.com>
2022-04-04 13:01:32 -0300

    Tests CoursesServive - parte 1

--- src/courses/courses.service.spec.ts
+++ src/courses/courses.service.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
 import { CoursesService } from './courses.service';
+import { Course } from './entities/course.entity';
+import { Tag } from './entities/tag.entity';
 
 describe('CoursesService', () => {
   let service: CoursesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CoursesService],
+      providers: [
+        CoursesService,
+        { provide: Connection, useValue: {} },
+        { provide: getRepositoryToken(Course), useValue: {} },
+        { provide: getRepositoryToken(Tag), useValue: {} },
+      ],
     }).compile();
 
     service = module.get<CoursesService>(CoursesService);
