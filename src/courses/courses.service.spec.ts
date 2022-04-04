a829330a864f86bb894eb4a81c2a0f907601b493
Aluizio Developer <aluiziodeveloper@gmail.com>
2022-04-04 17:38:07 -0300

    Tests CoursesServive - parte 2

--- src/courses/courses.service.spec.ts
+++ src/courses/courses.service.spec.ts
@@ -1,27 +1,53 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
-import { Connection } from 'typeorm';
+import { Connection, Repository } from 'typeorm';
 import { CoursesService } from './courses.service';
 import { Course } from './entities/course.entity';
 import { Tag } from './entities/tag.entity';
 
+type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
+
+const createMockRepository = <T = any>(): MockRepository<T> => ({
+  findOne: jest.fn(),
+});
+
 describe('CoursesService', () => {
   let service: CoursesService;
+  let courseRepository: MockRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         CoursesService,
         { provide: Connection, useValue: {} },
-        { provide: getRepositoryToken(Course), useValue: {} },
-        { provide: getRepositoryToken(Tag), useValue: {} },
+        {
+          provide: getRepositoryToken(Course),
+          useValue: createMockRepository(),
+        },
+        { provide: getRepositoryToken(Tag), useValue: createMockRepository() },
       ],
     }).compile();
 
     service = module.get<CoursesService>(CoursesService);
+    courseRepository = module.get<MockRepository>(getRepositoryToken(Course));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('findOne', () => {
+    describe('buscar curso pelo ID', () => {
+      it('deve retornar o objeto Course', async () => {
+        const courseId = '1';
+        const expectedCourse = {};
+
+        courseRepository.findOne.mockReturnValue(expectedCourse);
+        const course = await service.findOne(courseId);
+        expect(course).toEqual(expectedCourse);
+      });
+
+      it('deve retornar um NotFoundException', () => {});
+    });
   });
 });
