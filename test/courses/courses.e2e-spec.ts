64c729cc748f53f6bf22de4c2a2f4a5ab88738cf
Aluizio Developer <aluiziodeveloper@gmail.com>
2022-04-05 09:35:14 -0300

    Tests e2e - docker-compose dbtest

--- test/courses/courses.e2e-spec.ts
+++ test/courses/courses.e2e-spec.ts
@@ -2,13 +2,23 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import {CoursesModule} from '../../src/courses/courses.module'
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 describe('Courses: /courses', () => {
   let app: INestApplication;
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [CoursesModule],
+      imports: [CoursesModule, TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5433,
+      username: 'postgres',
+      password: 'docker',
+      database: 'testdb',
+      autoLoadEntities: true,
+      synchronize: true,
+    })],
     }).compile();
 
     app = moduleFixture.createNestApplication();
